24661d5e01f03234f37887bec9ea7bf362efbe40
MandySpaan <mandyspaan94@gmail.com>
2024-10-01 19:05:38 +0200

    style: ReservationModal

--- src/components/Modals/ReservationModal/ReservationModal.tsx
+++ src/components/Modals/ReservationModal/ReservationModal.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { reservation } from "../../../api/accessApicalls";
+import "./ReservationModal.css";
 import "../Modals.css";
 
 interface ModalProps {
