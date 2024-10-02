8edfb9cdf05665be9553867dfbeed19810f45b85
MandySpaan <mandyspaan94@gmail.com>
2024-10-02 13:13:06 +0200

    feat: AdminWelcome page

--- src/pages/Admin/AdminWelcome/AdminWelcome.tsx
+++ src/pages/Admin/AdminWelcome/AdminWelcome.tsx
@@ -1,7 +1,18 @@
+import React from "react";
 import "./AdminWelcome.css";
 
-const AdminWelcome = () => {
-  return <h1>AdminWelcome</h1>;
+const AdminWelcome: React.FC = () => {
+  return (
+    <div className="welcome-admin-container">
+      <h1>Welcome Las Naves Admin</h1>
+      <p>
+        Here, you can find information about users, rooms, access history. You
+        can also create and find reports. If you want to return to the regular
+        Las Naves website, you can do that by simply clicking on {}
+        <i>Leave Admin View</i> in the navigation bar.
+      </p>
+    </div>
+  );
 };
 
 export default AdminWelcome;
